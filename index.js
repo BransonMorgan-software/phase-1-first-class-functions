88adf24fb6715f183642e1e11d8109314e0e884c
Branson Morgan <bransox@comcast.net>
2022-08-10 18:44:27 -0400

    return functions

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+
 function receivesAFunction(spy) {
     return spy();
+}
+function returnsANamedFunction() {
+    return function spy(){
+        return ('to not deeply equal');
+}}
+function returnsAnAnonymousFunction () {
+    return function() {
+        return ('branson')
+    }
 }
